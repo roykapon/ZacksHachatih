a33f304b3c3280513872c41ee2dfc791f4df10a7
HochItay <itayhoch1212@gmail.com>
2024-02-02 12:01:30 +0200

    stuff for katz

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from api import ArazimBattlesBot, Emote, Exceptions, Monkeys
+from api import ArazimBattlesBot, Emote, Exceptions, Monkeys, Maps
 
 # Only pick the cool emotes of course
 EMOTES = [Emote.NONE, Emote.COOL, Emote.LAUGHING, Emote.LOVE]
@@ -12,23 +12,118 @@ UPGRADES = {
     Monkeys.BOMB_TOWER : (1,0)
 }
 
+LOCATIONS = {
+    Maps.YELLOW_BRICK: [
+        [230, 448],
+        [430, 176],
+        [345, 182],
+        [262, 256],
+        [282, 445],
+        [306, 219],
+        [248, 186],
+        [394, 137],
+        [323, 264],
+        [325, 419],
+        [372, 223],
+        [225, 141],
+        [153, 383],
+    ],
+    Maps.TEMPLE: [
+        [74, 531],
+        [166, 420],
+        [127, 483],
+        [227, 378],
+        [268, 312],
+        [333, 250],
+        [178, 480],
+        [282, 373],
+        [65, 474],
+        [227, 438],
+        [168, 367],
+    ],
+    Maps.SHAPES: [
+        [212, 282],
+        [122, 350],
+        [68, 568],
+        [184, 371],
+        [178, 318],
+        [236, 401],
+        [122, 287],
+        [71, 167],
+    ],
+    Maps.INTERCHANGE: [
+        [216, 272],
+        [146, 415],
+        [289, 490],
+        [318, 289],
+        [106, 348],
+        [207, 364],
+        [256, 346],
+        [196, 485],
+    ],
+}
+MONKEY_PREFERENCE = [Monkeys.DART_MONKEY, Monkeys.NINJA_MONKEY, Monkeys.SNIPER_MONKEY, Monkeys.SUPER_MONKEY]
+
 class MyBot(ArazimBattlesBot):
     monkey_count = 0
     monkey_levels = []
     attempted_position = 0
     emote_index = 0
 
+    monkey_types = []
+
     def setup(self) -> None:
         self.context.ban_monkey(Monkeys.NINJA_MONKEY)
 
+    def get_monkey(self) -> Monkeys:
+        if self.context.get_current_time() == 0:
+            return
+        banned = set(self.context.get_banned_monkeys())
+        unbanned = list(set(MONKEY_PREFERENCE) - banned)
+        chosen = min(unbanned, key=lambda m: MONKEY_PREFERENCE.index(m))
+        return chosen
+
+    def place(self):
+        self.context.log_info(self.context.get_map())
+        positions = LOCATIONS[self.context.get_map()]
+        m_type = self.get_monkey()
+        result = self.context.place_monkey(
+            m_type,
+            (
+                positions[self.attempted_position][0],
+                positions[self.attempted_position][1],
+            )
+            if self.attempted_position < len(positions)
+            else (
+                (24 * self.attempted_position) % 400 + 24,
+                200 + 24 * (24 * self.attempted_position) // 400,
+            ),
+        )
+        if result == Exceptions.OK:
+            self.monkey_count += 1
+            self.monkey_levels.append([0,0])
+            self.monkey_types.append(m_type)
+            
+        elif (
+            result == Exceptions.OUT_OF_MAP
+            or result == Exceptions.TOO_CLOSE_TO_BLOON_ROUTE
+            or result == Exceptions.TOO_CLOSE_TO_OTHER_MONKEY
+        ):
+            self.context.log_warning(f"Couldn't place monkey because of: {result}")
+            self.attempted_position += 1
+
     def upgrade(self, monkey_index):
-        m_type = Monkeys.SNIPER_MONKEY
+        m_type = self.monkey_types[monkey_index]
         if self.monkey_levels[monkey_index][0] < UPGRADES[m_type][0]:
             if self.context.upgrade_monkey(monkey_index, True):
                 self.monkey_levels[monkey_index][0] += 1
         elif self.monkey_levels[monkey_index][1] < UPGRADES[m_type][1]:
             if self.context.upgrade_monkey(monkey_index, False):
                 self.monkey_levels[monkey_index][1] += 1
+
+    def place_and_upgrade(self)
+        self.place()
+    
 
     def run(self) -> None:
         if self.context.get_current_time() % 5 == 0:
