3c5ed2f835e68688a65b86cbdcf0c2033f28eba2
HochItay <itayhoch1212@gmail.com>
2024-02-02 14:03:55 +0200

    stuff

--- main.py
+++ main.py
@@ -81,9 +81,10 @@ class MyBot(ArazimBattlesBot):
         positions = LOCATIONS[self.context.get_map()]
         if self.attempted_position >= len(positions["dart"]):
             temp = self.attempted_position % len(positions["dart"]) + 2
-            pos = (positions["tack"][temp][0], positions["tack"][temp][1])
-            if self.attempted_position >= len(positions["tack"]):
+            if temp >= len(positions["tack"]):
                 pos = ((24 * self.attempted_position) % 400 + 24, 200 + 24 * (24 * self.attempted_position) // 400)
+            pos = (positions["tack"][temp][0], positions["tack"][temp][1])
+            
         else:
             pos = (positions["dart"][self.attempted_position][0], positions["dart"][self.attempted_position][1])
         if self.attempted_position < 2:
